f6de905eea77ac2d02ba2a806a53fcd4b9a3faf9
bdvonarb <benjamin-vonarb@uiowa.edu>
2021-11-16 22:59:31 -0600

    Add table row component

---
+++ oops-all-ees-lab3/src/components/poll-table-row.js
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import '../styles/fa/css/all.css'
+
+const PollTableRow = ({ pollTitle, pollAuthor, pollID, signedin, deleteCallback, children}) => {
+    return (
+        <tr>
+            <td style={{verticalAlign:'middle'}}>{pollTitle}</td>
+            <td style={{verticalAlign:'middle'}}>{pollAuthor}</td>
+            <td style={{verticalAlign:'middle'}}>
+                <button type="button" className="btn btn-primary" style={{marginLeft:'5px'}}>
+                    <i className="far fa-eye"></i>
+                </button>
+                {signedin === "true" && <>
+                <button type="button" className="btn btn-success" style={{marginLeft:'5px'}}>
+                    <i className="far fa-edit"></i>
+                </button>
+                <button type="button" onClick={ () => deleteCallback(pollID)} className="btn btn-danger" style={{marginLeft:'5px'}}>
+                    <i className="far fa-trash-alt"></i>
+                </button>
+                </>
+                }
+            </td>
+        </tr>
+    )
+}
+
+export default PollTableRow
