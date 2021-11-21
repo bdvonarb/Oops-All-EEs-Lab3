1dbbaee4f9243cdd34866baddb2e60e00a1630b6
Mira Peckler <mirapeckler14@gmail.com>
2021-11-21 14:06:06 -0600

    Bare bones admin log in needs fixed

---
+++ oops-all-ees-lab3/src/pages/log-in.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const Loginforadmins = () => {
+    return (
+<form>
+  <label for="fname">First name:</label><br />
+  <input type="text" id="fname" name="fname" /><br />
+  <label for="lname">Last name:</label><br />
+  <input type="text" id="lname" name="lname" />
+</form>
+    )
+}
+export default Loginforadmins
