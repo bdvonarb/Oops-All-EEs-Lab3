d7b36d6b62c32931bab4cc3f83bfd260cf39304a
nichole <nichole-winter@uiowa.edu>
2021-11-22 07:24:07 -0600

    editted nav bar, tabs still need links

--- oops-all-ees-lab3/src/pages/index.js
+++ oops-all-ees-lab3/src/pages/index.js
@@ -1,15 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
 import {Link} from 'gatsby'
+import NavBar from '../components/navbar'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
+      <NavBar></NavBar>
       <title>Oops All EEs</title>
       <h1>Hi Nichole</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <Link to="/poll-list">Poll List</Link>
+      
+
     </main>
   )
 }
