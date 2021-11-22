afdc8aa14e14b75f68973e64c140b343bee6f27a
JaxSV <shawvannatta@uiowa.edu>
2021-11-22 14:11:21 -0600

    parsing date

--- oops-all-ees-lab3/src/components/time-slot-picker.js
+++ oops-all-ees-lab3/src/components/time-slot-picker.js
@@ -1,29 +1,49 @@
 import * as React from 'react'
+import { render } from 'react-dom'
 import '../styles/fa/css/all.css'
 
-const TimeSlotPicker = ({startDate, endDate, selectCallback, children}) => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    {
-                        
-                    }
+class TimeSlotPicker extends React.Component{
+    //const  = ({startDate, endDate, selectCallback, children}) => 
+
+    constructor(props){
+        super(props);
+        var sdate = new Date(props.startDate);
+        var edate = new Date(props.endDate);
+        var polllength = (Math.abs(props.startDate-props.endDate)/(1000*60*60*24))+1;
+        const columnname = []
+   
+        const months = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
+        const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+        if(sdate.getUTCMonth()==edate.getUTCMonth()){
+            var month = months[sdate.getUTCMonth()]
+        } else if(sdate.getUTCMonth()==edate.getUTCMonth()) {
+            
+        }
 
 
+        for(let i = 0; i<polllength-1; i++){
+            columnname[i]=days[sdate.getUTCDay()+i]+" "+month+" "+(sdate.getUTCDate()+i);
+        }
 
-                    <th style={{verticalAlign:'middle'}}>Poll</th>
-                    <th style={{verticalAlign:'middle'}}>Author</th>
-                    <th style={{verticalAlign:'middle'}}>Actions
-                        <button type="button" className="btn btn-info" onClick={this.newPoll} style={{marginLeft:'5px',float:'right'}}>
-                            <i className="fas fa-plus" style={{marginRight:"10px"}}></i>New
-                        </button>
-                    </th>
-                    
-                </tr>
-            </thead>
-        </table>
-    )
+        console.log(columnname)
+
+    }
+
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        {
+
+                        }
+                        <th style={{verticalAlign:'middle'}}>P0ll</th>
+                    </tr>
+                </thead>
+            </table>
+        )
+    }
 }
 
 export default TimeSlotPicker
