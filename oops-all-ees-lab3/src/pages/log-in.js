1e7bf2c816dd30dafa21c623cc2cf64d2f819d15
Mira Peckler <mirapeckler14@gmail.com>
2021-11-24 16:31:02 -0600

    Changed from F/L name to email and password prompt

--- oops-all-ees-lab3/src/pages/log-in.js
+++ oops-all-ees-lab3/src/pages/log-in.js
@@ -3,10 +3,10 @@ import * as React from "react"
 const Loginforadmins = () => {
     return (
 <form>
-  <label for="fname">First name:</label><br />
-  <input type="text" id="fname" name="fname" /><br />
-  <label for="lname">Last name:</label><br />
-  <input type="text" id="lname" name="lname" />
+  <label for="email address">Email address:</label><br />
+  <input type="text" id="emailaddress" name="emailaddress" /><br />
+  <label for="password">Password:</label><br />
+  <input type="text" id="password" name="password" />
 </form>
     )
 }
