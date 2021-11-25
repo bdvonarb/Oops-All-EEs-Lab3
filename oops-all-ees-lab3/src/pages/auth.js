26334d6d9ea530776f6608055f51055aa805952c
Mira Peckler <mirapeckler14@gmail.com>
2021-11-24 19:14:09 -0600

    Authentication section for log in page

---
+++ oops-all-ees-lab3/src/pages/auth.js
@@ -0,0 +1,32 @@
+import * as React from "react"
+
+export const Broswer = () => typeof window !=="undefined"
+
+export const getUser = () =>
+    Broswer() && window.localstorage.getItem("gatsbyuser")
+    ? JSON.parse(window.localstorage.getItem("gatsbyuser"))
+    : {}
+
+    const setUser = User => 
+        window.localstorage.setItem("gatsbyuser", JSON.stringify(User))
+
+export const handlelogin = ({email, password}) => {
+    if (email === 'admin@oopsallees.com' && password === 'guest') {
+        return setUser({
+            email: 'admin',
+            name: 'administator',
+        })
+    }
+    return false
+}
+
+export const isloggedin = () => {
+    const User = getUser()
+     
+    return !!User.email
+}
+
+export const logout = callback => {
+    setUser({})
+    callback()
+}
