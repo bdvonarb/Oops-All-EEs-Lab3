a7165707728462c68c3e3a0b6a1d1294423cac78
Mira Peckler <mirapeckler14@gmail.com>
2021-11-24 18:23:43 -0600

    Added submit button

--- oops-all-ees-lab3/src/pages/log-in.js
+++ oops-all-ees-lab3/src/pages/log-in.js
@@ -3,10 +3,15 @@ import * as React from "react"
 const Loginforadmins = () => {
     return (
 <form>
+
   <label for="email address">Email address:</label><br />
   <input type="text" id="emailaddress" name="emailaddress" /><br />
+
   <label for="password">Password:</label><br />
-  <input type="text" id="password" name="password" />
+  <input type="text" id="password" name="password" /><br />
+
+  <input type="submit" value="Submit" /><br />
+        
 </form>
     )
 }
