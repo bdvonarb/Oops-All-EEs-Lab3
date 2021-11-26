acf22bb93ba98ca96f39ae9cbee518280202dae4
Mira Peckler <mirapeckler14@gmail.com>
2021-11-26 16:31:05 -0600

    Adding css components such as text color

--- oops-all-ees-lab3/src/pages/log-in.js
+++ oops-all-ees-lab3/src/pages/log-in.js
@@ -1,18 +1,27 @@
 import * as React from "react"
 
+const Mainstyle = {
+    color: "#00ace3",
+    width: "382px",
+    fontFamily: "Times, Times New Roman, serif",
+
+    
+}
 const Loginforadmins = () => {
     return (
-<form>
+      <main style={Mainstyle}>
+      <><title>Login</title><form>
 
-  <label for="email address">Email address:</label><br />
-  <input type="text" id="emailaddress" name="emailaddress" /><br />
+        <label for="email address">Email address:</label><br />
+        <input type="text" id="emailaddress" name="emailaddress" /><br />
 
-  <label for="password">Password:</label><br />
-  <input type="text" id="password" name="password" /><br />
+        <label for="password">Password:</label><br />
+        <input type="text" id="password" name="password" /><br />
 
-  <input type="submit" value="Submit" /><br />
-        
-</form>
+        <input type="submit" value="Submit" /><br />
+
+      </form></>
+      </main>
     )
 }
 export default Loginforadmins
