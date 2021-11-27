a889699626ddc0c547951865b94767164c2acd4d
Mira Peckler <mirapeckler14@gmail.com>
2021-11-27 13:33:48 -0600

    Added log in and background color

--- oops-all-ees-lab3/src/pages/log-in.js
+++ oops-all-ees-lab3/src/pages/log-in.js
@@ -1,21 +1,30 @@
 import * as React from "react"
 
+const Bodystyle = {
+  backgroundColor: "blue",
+  height: "800px",
+  margin: "-100px 0px 0px 0px",
+}
+
 const Mainstyle = {
     color: "#1be6e2",
     width: "350px",
     margin: "auto",
     margin: "100px 0px 0px 450px",
     padding: "90px",
-    background: "#23463f",
+    backgroundColor: "#23463f",
     fontFamily: "Times, Times New Roman, serif",
     fontSize: "18px",
     borderRadius: "20px",
 }  
+
+
 const Loginforadmins = () => {
     return (
       <>
+      <body style={Bodystyle}>
        <main style={Mainstyle}>
-         <title>Login</title>
+         <h1>Login</h1>
          <form>
 
         <label for="email address">Email address:</label><br />
@@ -28,6 +37,7 @@ const Loginforadmins = () => {
 
       </form>
       </main>
+      </body>
       </>   
     )
 }
